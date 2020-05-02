9272696a94549e8ea2c41b77e5589b5f22742cd9
Ankur Soni <ankur.soni@outlook.com>
2020-05-02 18:34:51 +1000

    Minor changes

--- scripts/setup-coredns.sh
+++ scripts/setup-coredns.sh
@@ -15,8 +15,20 @@ kubectl get all --all-namespaces --kubeconfig worker/configs/admin.kubeconfig
 
 # give time for coredns to start
 echo "Sleeping to give time for coredns to start"
-sleep 40
+sleep 30
 
-echo -e "\nDisplaying 'nslookup kubernetes' output"
+echo -e "\nExecuting 'kubectl run --image=busybox:1.28 busybox -- nslookup kubernetes' output"
 # execute nslookup command in the busybox container to test dns
-kubectl run -it --rm --kubeconfig worker/configs/admin.kubeconfig --image=busybox:1.28 busybox -- nslookup kubernetes
+kubectl run --kubeconfig worker/configs/admin.kubeconfig --image=busybox:1.28 busybox -- nslookup kubernetes
+
+# give time for busybox to start
+echo "Sleeping to give time for busybox to start"
+sleep 10
+
+echo "Displaying 'kubectl logs busybox' output"
+# get the results from test dns
+kubectl logs busybox --kubeconfig worker/configs/admin.kubeconfig
+
+echo "Displaying 'kubectl delete pod busybox' output"
+# cleanup of busybox pod
+kubectl delete pod busybox --kubeconfig worker/configs/admin.kubeconfig
